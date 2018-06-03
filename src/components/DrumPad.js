0184a14b5760b7ddcec379899874ffb379d54adf
Bogdan5 <bogdan5x@gmail.com>
2018-06-02 21:27:12 -0400

    'Comment'

--- src/components/DrumPad.js
+++ src/components/DrumPad.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
-import Display from './components/Display.js';
+import DrumPad from './components/DrumPad.js';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div id="drum-machine" className="Display">
+      <div className="drum-pad">
 
       </div>
     );
