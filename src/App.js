620f05687505e642b3933c16a2b676ac1cd60586
Bogdan5 <bogdan5x@gmail.com>
2018-06-02 23:15:29 -0400

    'Comment'

--- src/App.js
+++ src/App.js
@@ -3,10 +3,31 @@ import Display from './components/Display.js';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { drumPads: [
+      { name: 'q' },
+      { name: 'w' },
+      { name: 'e' },
+      { name: 'a' },
+      { name: 's' },
+      { name: 'd' },
+      { name: 'z' },
+      { name: 'x' },
+      { name: 'c' },
+    ], };
+  };
+
   render() {
     return (
       <div id="drum-machine" className="Display">
-
+        {this.state.drumPads.map((item, index)=><DrumPad/>)}
+        <div id="display">
+          <SliderButton/>
+          <Display/>
+          <Slider/>
+          <SliderButton/>
+        </div>
       </div>
     );
   }
