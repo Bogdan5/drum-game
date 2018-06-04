e11132980eb29f3cb65a6a36409a92f7f25ea1ac
Bogdan5 <bogdan5x@gmail.com>
2018-06-03 22:33:02 -0400

    'Comment'

--- src/App.js
+++ src/App.js
@@ -6,22 +6,27 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = { drumPads: [
-      { name: 'q' },
-      { name: 'w' },
-      { name: 'e' },
-      { name: 'a' },
-      { name: 's' },
-      { name: 'd' },
-      { name: 'z' },
-      { name: 'x' },
-      { name: 'c' },
+      { name: 'q', url: 'http://www.peter-weinberg.com/files/9114/8176/9796/Kick_n_Hat.wav', },
+      { name: 'w', url: 'http://www.peter-weinberg.com/files/9314/8176/9796/Heater-6.wav', },
+      { name: 'e', url: 'http://www.peter-weinberg.com/files/9114/8176/9796/Kick_n_Hat.wav', },
+      { name: 'a', url: 'http://www.peter-weinberg.com/files/9314/8176/9796/Heater-6.wav', },
+      { name: 's', url: 'http://www.peter-weinberg.com/files/9114/8176/9796/Kick_n_Hat.wav', },
+      { name: 'd', url: 'http://www.peter-weinberg.com/files/9314/8176/9796/Heater-6.wav', },
+      { name: 'z', url: 'http://www.peter-weinberg.com/files/9114/8176/9796/Kick_n_Hat.wav', },
+      { name: 'x', url: 'http://www.peter-weinberg.com/files/9314/8176/9796/Heater-6.wav', },
+      { name: 'c', url: 'http://www.peter-weinberg.com/files/9114/8176/9796/Kick_n_Hat.wav', },
     ], };
+  };
+
+  drumBeat = (letter) => {
+
   };
 
   render() {
     return (
       <div id="drum-machine" className="Display">
-        {this.state.drumPads.map((item, index)=><DrumPad/>)}
+        {this.state.drumPads.map((item, index)=><DrumPad clicker={this.drumBeat}
+        url={item.url}/>)};
         <div id="display">
           <SliderButton/>
           <Display/>
--- src/components/DrumPad.js
+++ src/components/DrumPad.js
@@ -3,10 +3,13 @@ import DrumPad from './components/DrumPad.js';
 import './App.css';
 
 class DrumPad extends Component {
+  clicker = () => this.props.clicker(this.props.letter);
+
   render() {
     return (
-      <div className="drum-pad">
-        <div id="display"></div>
+      <div className="drum-pad" onClick={this.clicker}>
+        {this.props.letter}
+        <audio src={this.props.url} className="clip" id={this.props.letter}></audio>
       </div>
     );
   }
