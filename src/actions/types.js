d0a478f5b322bf610a8b387ca9f07f2eaeb87dcf
Bogdan5 <bogdan5x@gmail.com>
2018-06-12 23:58:34 -0400

    'Comment'

--- src/actions/types.js
+++ src/actions/types.js
@@ -1,3 +1,7 @@
 export const DRUM_CLICKED = 'DRUM_CLICKED';
 export const SLIDER_MOVED = 'SLIDER_MOVED';
 export const START = 'START';
+
+export function drumBeat(name) {
+  return { nameButton: name };
+}
