514210887e8862bad935d03957922589559b8c21
Bogdan5 <bogdan5x@gmail.com>
2018-06-17 22:50:36 -0400

    'Comment'

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -2,11 +2,34 @@ import React, { Component } from 'react';
 import './App.css';
 
 class Slider extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { start: false, position: 0, left: 0 };
+  }
+
+  start = () => {
+    this.setState({ start: true });
+  };
+
+  end = () => {
+    this.setState({ start: false });
+  };
+
+  moving = () => {
+    if (this.state.start) {
+      //change ref
+      let inputRect =  ReactDOM.findDOMNode(this.refs['sliderButton'])
+      .getBoundingClientRect();
+      this.setState({ position: event.clientX - inputRect.left });
+    }
+  };
+
   render() {
     return (
       <div className="slider-container">
-        <div className="slider-button" draggable="true" onDragEnd={this.dragEnd}
-              onDragStart={this.dragStart}></div>
+        //change ref from string to function
+        <div ref="sliderButton" style={this.state.left} className="slider-button" onMouseDown={this.start}
+          onMouseUp={this.end} onMouseMove={this.moving}></div>
       </div>
     );
   }
