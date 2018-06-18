8eac608a12e355f19a58d28db85a732650fc58b9
Bogdan5 <bogdan5x@gmail.com>
2018-06-17 21:00:26 -0400

    'Comment'

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -4,9 +4,9 @@ import './App.css';
 class Slider extends Component {
   render() {
     return (
-      <div className="slider">
-        <div className="line"></div>
-        <div className="indicator"></div>
+      <div className="slider-container">
+        <div className="slider-button" draggable="true" onDragEnd={this.dragEnd}
+              onDragStart={this.dragStart}></div>
       </div>
     );
   }
